4204679dd303a4dd51617910e6a12dca6874db2e
mubonassi <115490684+mubonassi@users.noreply.github.com>
2026-03-24 17:37:49 +0000

    aaaaaaaaaaaa

---
+++ desafios/desafio7.py
@@ -0,0 +1,11 @@
+numero = int(input("Digite um número: "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+dobro = numero * 2
+metade = numero / 2
+
+print(f"Antecessor do {numero} é {antecessor}")
+print(f"Sucessor do {numero} é {sucessor}")
+print(f"Dobro do {numero} é {dobro}")
+print(f"Metade do {numero} é {metade}")
